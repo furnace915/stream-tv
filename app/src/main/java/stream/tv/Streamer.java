495d22b8e4b6f78fd5b53a7f171c0298dc367d42
Brent Velandra <brent.velandra@gmail.com>
2022-09-04 22:13:57 -0400

    require plan upgrade to stream current on-demand content

--- app/src/main/java/stream/tv/Streamer.java
+++ app/src/main/java/stream/tv/Streamer.java
@@ -2,12 +2,19 @@ package stream.tv;
 
 public class Streamer {
     public String play(Content content) {
-        return new StringBuilder()
-                .append("playing ")
-                .append(content.getClassification())
-                .append(": ")
-                .append(content.getSubclassification())
-                .append(" - ")
-                .append(content.getName()).toString();
+
+           if(content.getSubclassification() == "Rerun"){
+               return new StringBuilder()
+                       .append("playing ")
+                       .append(content.getClassification())
+                       .append(": ")
+                       .append(content.getSubclassification())
+                       .append(" - ")
+                       .append(content.getName()).toString();
+           }else {
+               return "upgrade your plan to access this content";
+           }
+
+
     }
 }
--- app/src/test/java/stream/tv/StreamerTest.java
+++ app/src/test/java/stream/tv/StreamerTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StreamerTest {
 
     @Test
-    void Given_Free_User_And_On_Demand_Rerun_When_Play_Then_Return_Streamed_Content() {
+    void Given_Free_Plan_When_Play_On_Demand_Rerun_Then_Provide_Streamed_Content() {
         Content content = new Content.ContentBuilder()
                 .classification("On-Demand")
                 .subclassification("Rerun")
@@ -18,6 +18,21 @@ public class StreamerTest {
         String actual = streamer.play(content);
 
         assertThat(actual).isEqualTo("playing On-Demand: Rerun - The Godfather");
+
+    }
+
+    @Test
+    void Given_Free_Plan_When_Play_On_Demand_Movie_Then_Block_Streamed_Content() {
+        Content content = new Content.ContentBuilder()
+                .classification("On-Demand")
+                .subclassification("Current")
+                .name("Star Wars XX")
+                .build();
+
+        Streamer streamer = new Streamer();
+        String actual = streamer.play(content);
+
+        assertThat(actual).isEqualTo("upgrade your plan to access this content");
 
     }
 }
