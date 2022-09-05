bfe39ca646e004a02206fc2f54d14f1624a942aa
Brent Velandra <brent.velandra@gmail.com>
2022-09-04 23:42:44 -0400

    extract constant

--- app/src/main/java/stream/tv/Streamer.java
+++ app/src/main/java/stream/tv/Streamer.java
@@ -4,9 +4,12 @@ import static stream.tv.Classification.LIVESTREAM;
 import static stream.tv.SubClassification.RERUN;
 
 public class Streamer {
+
+    private static final String REQUIRES_PLAN_UPGRADE_MESSAGE = "upgrade your plan to access this content";
+
     public String play(Content content) {
         if (LIVESTREAM.equals(content.getClassification())) {
-            return "upgrade your plan to access this content";
+            return REQUIRES_PLAN_UPGRADE_MESSAGE;
         }
 
         if (RERUN.equals(content.getSubclassification())) {
@@ -18,7 +21,7 @@ public class Streamer {
                     .append(" - ")
                     .append(content.getName()).toString();
         }
-        return "upgrade your plan to access this content";
+        return REQUIRES_PLAN_UPGRADE_MESSAGE;
 
     }
 }
